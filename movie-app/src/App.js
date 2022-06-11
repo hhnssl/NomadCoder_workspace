c5ac3d17cc3ec9a212e879ab3f4c3eb73d8325b7
hhnssl <losnah5h@gmail.com>
2022-06-11 20:10:07 +0900

    save making movie-app part.1 (didn't use other files)

---
+++ movie-app/src/App.js
@@ -0,0 +1,41 @@
+import {useState, useEffect} from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () =>{
+    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")).json();
+      setMovies(json.data.movies)
+      setLoading(false);
+  };
+
+  // just use only once
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? <h1>Loading....</h1> :  
+      <div>
+        {movies.map((item) => 
+          <div key={item.id}>
+            <img src={item.medium_cover_image} alt=""/>
+            <h2>{item.title}</h2>
+            <p>{item.summary}</p>
+            <ul>
+              {item.genres.map((genre) => (
+                <li key={genre}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    }
+
+      
+    </div>
+  );
+}
+
+export default App;
