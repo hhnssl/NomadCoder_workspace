10ea10a46c3dec9dc811822807fe8364f0ef3011
hhnssl <losnah5h@gmail.com>
2022-06-16 00:55:31 +0900

    완강

--- movie-app/src/routes/Detail.js
+++ movie-app/src/routes/Detail.js
@@ -1,22 +1,36 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { useState } from "react";
 
 function Detail() {
   const { id } = useParams();
   // console.log(id); // id 객체 반환
 
+  const [loading, setLoading] = useState(true);
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    // console.log(json);
+    setLoading(false);
   };
 
   useEffect(() => {
     getMovie();
   }, []);
 
-  return <h1>details~~</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>
+          lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,lorem,,,,
+        </h1>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
